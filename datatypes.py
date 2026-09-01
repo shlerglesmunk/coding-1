07822379a33560bdddb1667bc674587a0104c319
shlerglesmunk <ssmith188@boyslatin.net>
2026-09-01 13:37:44 +0000

    datatypings

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+String - characters that are wrapped within quotes
+"the 80s i think"
+"kill people!"
+Hashtags make the code you've written invisible to the computer.
+
+Integers are whole numbers that's          that's kinda it
+Floats are decimals                   that's it I guess ok
+
+Boolean is a datatype that is either true or fasle based on other variables
