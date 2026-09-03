44af5518c097458695a6d5ec43c30c37e3ec8df6
shlerglesmunk <ssmith188@boyslatin.net>
2026-09-03 13:44:46 +0000

    class variably activity thingy

---
+++ unit 1/variableActivity.py
@@ -0,0 +1,36 @@
+variables store data like a container as long as it is descriptive.
+
+evilmeter= 5
+
+print(evilmeter)
+
+variables can be letters and numbers but thats it.
+variables must start with a letter.
+variables cant be python keywords.
+
+
+
+
+
+activity thing
+
+applecount= 96076087698769
+Integers
+
+studentheads= 2
+also Integers
+
+payfortripinsurance= <false>
+boolean
+
+
+
+
+
+
+
+
+
+camel case - where everyWordIsACapitalLetter= 80
+
+snake case - underscores are spacebars_now= 84359843574936565783
