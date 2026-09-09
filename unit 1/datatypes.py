fd88e6899573f18cb5df95fa6e4c6cd9f6fe1e16
shlerglesmunk <ssmith188@boyslatin.net>
2026-09-09 13:18:17 +0000

    evil warmup review

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -7,3 +7,5 @@ Integers are whole numbers that's          that's kinda it
 Floats are decimals                   that's it I guess ok
 
 Boolean is a datatype that is either true or fasle based on other variables
+
+Operators give our data power to do stuff making them super important.
