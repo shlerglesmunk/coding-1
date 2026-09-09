b829c5cf4a570ef39b4d1eb27b625cea4ab01f43
shlerglesmunk <ssmith188@boyslatin.net>
2026-09-09 13:44:32 +0000

    operatortors

---
+++ unit 1/operators.py
@@ -0,0 +1,7 @@
+assignment operators - assigning values to variables
+key / value pairings
+
+Arithmetic operators - used to perform numerical calculations (uses intagers and floats)
+print(3 + 9739725079235)
+
+comparison operators - Compares if assets are the same or different and why they differ.
