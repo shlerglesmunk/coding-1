b7d1e794ab6def0d246fd609dbf69a15d834af58
shlerglesmunk <ssmith188@boyslatin.net>
2026-09-10 13:39:12 +0000

    operator notes

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -4,4 +4,14 @@ key / value pairings
 Arithmetic operators - used to perform numerical calculations (uses intagers and floats)
 print(3 + 9739725079235)
 
-comparison operators - Compares if assets are the same or different and why they differ.
+comparison operators - Compares if assets are the same or different and why they differ.``  
+
+print(2.0 == 2)
+
+print(300 != 100) 
+
+print(3 > 1 and 100 > 50) # logical operators
+
+print(3 > 1 or 100 > 50)
+
+print(not(3 > 1 and 100 > 50))
